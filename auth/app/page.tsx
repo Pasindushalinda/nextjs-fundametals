b57a6d2625981a686d45122579bcf18951a688e9
Pasindu Rathnayake <pshalinda5@gmail.com>
2026-03-26 11:41:26 +0530

    auth sign in

--- auth/app/page.tsx
+++ auth/app/page.tsx
@@ -1,5 +1,25 @@
 import { Button } from "@heroui/react";
+import * as actions from "@/actions";
+import { auth } from "@/auth/auth";
 
-export default function Home() {
-  return <Button>My Button</Button>;
+export default async function Home() {
+  const session = await auth();
+
+  return (
+    <div>
+      <form action={actions.signIn}>
+        <Button>SignIn</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button>Sign Out</Button>
+      </form>
+
+      {session?.user ? (
+        <div>{JSON.stringify(session.user)}</div>
+      ) : (
+        <div>Signed Out</div>
+      )}
+    </div>
+  );
 }
